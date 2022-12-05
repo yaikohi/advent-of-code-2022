9f074699ea74fcf445b2dcf72d60fccd2cc8c5be
yaikohi <erik.beem@hotmail.com>
2022-12-05 15:49:52 +0100

    Day04-2

--- day-04/src/lib.rs
+++ day-04/src/lib.rs
@@ -1,5 +1,5 @@
 pub fn process_part1(input: &str) -> String {
-    let mut result = 0;
+    let mut result: u16 = 0;
     let formatted_input = input.lines().collect::<Vec<&str>>().into_iter();
 
     for line in formatted_input {
@@ -13,10 +13,10 @@ pub fn process_part1(input: &str) -> String {
                     .collect::<Vec<&str>>()
                     .into_iter()
                     .map(|val| {
-                        let res = val.parse::<usize>().unwrap();
+                        let res = val.parse::<u16>().unwrap();
                         res
                     })
-                    .collect::<Vec<usize>>();
+                    .collect::<Vec<u16>>();
                 res
             })
             .collect::<Vec<_>>();
@@ -28,13 +28,39 @@ pub fn process_part1(input: &str) -> String {
         }
     }
 
-    dbg!(result);
-
     result.to_string()
 }
 
-pub fn process_part2(input: &str) -> &str {
-    input
+pub fn process_part2(input: &str) -> String {
+    let mut result: u16 = 0;
+    let formatted_input = input.lines().collect::<Vec<&str>>().into_iter();
+
+    for line in formatted_input {
+        let lines = line
+            .split(",")
+            .collect::<Vec<&str>>()
+            .into_iter()
+            .map(|slice| {
+                let res = slice
+                    .split("-")
+                    .collect::<Vec<&str>>()
+                    .into_iter()
+                    .map(|val| {
+                        let res = val.parse::<u16>().unwrap();
+                        res
+                    })
+                    .collect::<Vec<u16>>();
+                res
+            })
+            .collect::<Vec<_>>();
+
+            if !(lines[0][1] < lines[1][0] || lines[1][1] < lines[0][0]) {
+                dbg!(&lines);
+                result+=1
+            }
+    }
+
+    result.to_string()
 }
 
 #[cfg(test)]
@@ -49,9 +75,13 @@ mod tests {
         assert_eq!(result, "2");
     }
 
-    // #[test]
-    // fn p2_works() {
-    //     let result = process_part2(TEST_INPUT);
-    //     // assert_eq!(result, 4);
-    // }
+    #[test]
+    fn p2_works() {
+        let result = process_part2(TEST_INPUT);
+        assert_eq!(result, "4");
+    }
 }
+
+// 955 = too high
+// 526 incorrect
+// 917 correct
