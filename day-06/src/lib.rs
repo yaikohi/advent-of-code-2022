1d43e1c56f661b61a5d65c1d17321e8736017025
yaikohi <erik.beem@hotmail.com>
2022-12-06 14:19:46 +0100

    Solved day-06 p2

--- day-06/src/lib.rs
+++ day-06/src/lib.rs
@@ -22,34 +22,71 @@ pub fn process_part1(input: &str) -> String {
             result = (char as u16) + 4;
             break;
         }
-
     }
     result.to_string()
 }
 
-// pub fn process_part2(input: &str) -> String {
-//     let mut result: u16 = 0;
-//     result.to_string()
-// }
+pub fn process_part2(input: &str) -> String {
+    let mut result: u16 = 0;
+    let res = input
+        .split("")
+        .filter(|c| !(c.is_empty()))
+        .collect::<Vec<&str>>();
+    for char in 0..(res.len() - 14) {
+        let one = res[char];
+        let two = res[char + 1];
+        let three = res[char + 2];
+        let four = res[char + 3];
+        let five = res[char + 4];
+        let six = res[char + 5];
+        let seven = res[char + 6];
+        let eight = res[char + 7];
+        let nine = res[char + 8];
+        let ten = res[char + 9];
+        let eleven = res[char + 10];
+        let twelve = res[char + 11];
+        let thirteen = res[char + 12];
+        let fourteen = res[char + 13];
+
+        let original_slice = [
+            one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, thirteen,
+            fourteen,
+        ];
+        let unique_slice = [
+            one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, thirteen,
+            fourteen,
+        ]
+        .into_iter()
+        .unique()
+        .collect::<Vec<&str>>();
+
+        if original_slice.len() == unique_slice.len() {
+            result = (char as u16) + 14;
+            break;
+        }
+    }
+    result.to_string()
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     const TEST_INPUT: &str = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
-
     #[test]
     fn p1_works() {
         let result = process_part1(TEST_INPUT);
         assert_eq!(result, "7");
     }
 
-    // #[test]
-    // fn p2_works() {
-    //     let result = process_part2(TEST_INPUT);
-    //     assert_eq!(result, "");
-    // }
+    #[test]
+    fn p2_works() {
+        let result = process_part2(TEST_INPUT);
+        assert_eq!(result, "19");
+    }
 }
 
-// 1829 is too low
-// 1833 - correct!
+// p1: 1829 is too low
+// p1: 1833 - correct!
+
+// p2: 3425 - correct!
